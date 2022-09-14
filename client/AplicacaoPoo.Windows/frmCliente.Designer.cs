9defd7d56b43c93275fc69b81edf74d4cc901819
Diogo Zonta <diogo_alveszonta2015@hotmail.com>
2022-09-13 21:53:09 -0300

    Adicionado frmCliente.

---
+++ client/AplicacaoPoo.Windows/frmCliente.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AplicacaoPoo.Windows
+{
+    partial class frmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblValorCredito = new System.Windows.Forms.Label();
+            this.lblValorDebito = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtValorDebito = new System.Windows.Forms.TextBox();
+            this.txtValorCredito = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.Location = new System.Drawing.Point(34, 31);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(59, 21);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "NOME:";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCpf.Location = new System.Drawing.Point(34, 69);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(40, 21);
+            this.lblCpf.TabIndex = 1;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // lblValorCredito
+            // 
+            this.lblValorCredito.AutoSize = true;
+            this.lblValorCredito.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblValorCredito.Location = new System.Drawing.Point(34, 110);
+            this.lblValorCredito.Name = "lblValorCredito";
+            this.lblValorCredito.Size = new System.Drawing.Size(125, 21);
+            this.lblValorCredito.TabIndex = 2;
+            this.lblValorCredito.Text = "Valor de Crédito:";
+            // 
+            // lblValorDebito
+            // 
+            this.lblValorDebito.AutoSize = true;
+            this.lblValorDebito.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblValorDebito.Location = new System.Drawing.Point(34, 148);
+            this.lblValorDebito.Name = "lblValorDebito";
+            this.lblValorDebito.Size = new System.Drawing.Size(120, 21);
+            this.lblValorDebito.TabIndex = 3;
+            this.lblValorDebito.Text = "Valor de Débito:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(184, 33);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(184, 71);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 23);
+            this.txtCpf.TabIndex = 5;
+            // 
+            // txtValorDebito
+            // 
+            this.txtValorDebito.Location = new System.Drawing.Point(184, 146);
+            this.txtValorDebito.Name = "txtValorDebito";
+            this.txtValorDebito.Size = new System.Drawing.Size(100, 23);
+            this.txtValorDebito.TabIndex = 6;
+            // 
+            // txtValorCredito
+            // 
+            this.txtValorCredito.Location = new System.Drawing.Point(184, 112);
+            this.txtValorCredito.Name = "txtValorCredito";
+            this.txtValorCredito.Size = new System.Drawing.Size(100, 23);
+            this.txtValorCredito.TabIndex = 7;
+            // 
+            // frmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 197);
+            this.Controls.Add(this.txtValorCredito);
+            this.Controls.Add(this.txtValorDebito);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblValorDebito);
+            this.Controls.Add(this.lblValorCredito);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblNome);
+            this.Name = "frmCliente";
+            this.Text = "frmCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblNome;
+        private Label lblCpf;
+        private Label lblValorCredito;
+        private Label lblValorDebito;
+        private TextBox txtNome;
+        private TextBox txtCpf;
+        private TextBox txtValorDebito;
+        private TextBox txtValorCredito;
+    }
+}
